1740e7c029953e64c44a3622c51e5a89964da266
samwyz <sambrgr@gmail.com>
2016-07-19 11:38:46 -0500

    contants

---
+++ NewsApp/app/src/main/java/samcorp/newsapp/Constants.java
@@ -0,0 +1,17 @@
+package samcorp.newsapp;
+
+/**
+ * Created by samwyz on 7/18/16.
+ */
+public class Constants {
+
+    public static final String GUARDIAN_POLITICS =
+            "http://content.guardianapis.com/search?order-by=newest&section=politics&limit=10&api-key=1caa0efc-fb6b-4b50-a2d9-20aa44d13a81";
+
+    public static final String GUARDIAN_CULTURE =
+            "http://content.guardianapis.com/search?order-by=newest&section=culture&limit=10&api-key=1caa0efc-fb6b-4b50-a2d9-20aa44d13a81";
+
+    public static final String GUARDIAN_FILM =
+            "http://content.guardianapis.com/search?order-by=newest&section=film&limit=10&format=json&show-fields=thumbnail&api-key=1caa0efc-fb6b-4b50-a2d9-20aa44d13a81";
+
+}
